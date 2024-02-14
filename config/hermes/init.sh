c2bc8b34d65e474ef046e8cceaa9aa6be7e6b691
0x4r45h <0x4r45h@gmail.com>
2024-02-14 18:52:50 +0330

    hermes works now

--- config/hermes/init.sh
+++ config/hermes/init.sh
@@ -1,38 +1,37 @@
 #!/bin/bash
 #mkdir -p ~/.namada-shared/chain-a && mkdir -p ~/.namada-shared/chain-b
 
-
-export CHAIN_A_ID=$(curl -s namada-1:26657/status | jq '.result.node_info.network')
-export CHAIN_B_ID=$(curl -s chain-b:26657/status | jq '.result.node_info.network')
+export CHAIN_A_ID=$(curl -s namada-1:26657/status | jq '.result.node_info.network' | sed 's/"//g')
+export CHAIN_B_ID=$(curl -s chain-b:26657/status | jq '.result.node_info.network' | sed 's/"//g')
+export CHAIN_A_NAM_ADDR=$(awk 'NR==1' /root/.namada-shared/tokens-addresses)
+export CHAIN_B_NAM_ADDR=$(awk 'NR==1' /root/.namada-shared/chain-b-token-addrs)
 export HERMES_CONFIG=/root/.hermes/config.toml
-# TODO : make relayer account on each chain init script
-# TODO : share each chain wallet.toml for hermes e.g:
-namadaw gen --alias relayer --unsafe-dont-encrypt
-cp ~/.local/share/namada/$CHAIN_A_ID/wallet.toml ~/.namada-shared/chain-a/wallet.toml
 
-namadaw gen --alias relayer --unsafe-dont-encrypt
-cp ~/.local/share/namada/$CHAIN_B_ID/wallet.toml ~/.namada-shared/chain-b/wallet.toml
+if [ ! -f "/root/.hermes_channel" ]; then
 
-# TODO : fund relayer accounts
-namadac transfer --source namada-1-wallet --target relayer --amount 500 --token NAM
-namadac transfer --source chain-b-wallet --target relayer --amount 500 --token NAM
+  # share each chain wallet.toml for hermes
+  mkdir -p /root/wallets/wallet-a && mkdir -p /root/wallets/wallet-b
+  curl -o /root/wallets/wallet-a/wallet.toml http://namada-1:31222/wallet.toml
+  curl -o /root/wallets/wallet-b/wallet.toml http://chain-b:31222/wallet.toml
 
+  sed -e "s/_CHAIN_A_ID_/$CHAIN_A_ID/g" \
+    -e "s/_CHAIN_B_ID_/$CHAIN_B_ID/g" \
+    -e "s/_NAM_A_ADDR_/$CHAIN_A_NAM_ADDR/g" \
+    -e "s/_NAM_B_ADDR_/$CHAIN_B_NAM_ADDR/g" \
+    /root/config-template.toml >"$HERMES_CONFIG"
 
-echo $CHAIN_A_ID
-echo $CHAIN_B_ID
+  hermes --config $HERMES_CONFIG keys add --chain "$CHAIN_A_ID" --key-file /root/wallets/wallet-a/wallet.toml
+  hermes --config $HERMES_CONFIG keys add --chain "$CHAIN_B_ID" --key-file /root/wallets/wallet-b/wallet.toml
 
-hermes --config $HERMES_CONFIG keys add --chain "$CHAIN_A_ID" --key-file /root/.namada-shared/chain-a/wallet.toml
-hermes --config $HERMES_CONFIG keys add --chain "$CHAIN_B_ID" --key-file /root/.namada-shared/chain-b/wallet.toml
-# TODO : update chain ids
-# TODO : update nam address
+  CHANNEL_CREATING_LOG=$(hermes --config $HERMES_CONFIG \
+    create channel \
+    --a-chain $CHAIN_A_ID \
+    --b-chain $CHAIN_B_ID \
+    --a-port transfer \
+    --b-port transfer \
+    --new-client-connection --yes)
+  echo $CHANNEL_CREATING_LOG
+  echo $CHANNEL_CREATING_LOG >>/root/.hermes_channel
 
+fi
 hermes --config $HERMES_CONFIG start
-
-
-hermes --config $HERMES_CONFIG \
-  create channel \
-  --a-chain $CHAIN_A_ID \
-  --b-chain $CHAIN_B_ID \
-  --a-port transfer \
-  --b-port transfer \
-  --new-client-connection --yes
